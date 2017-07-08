4aea00a3f346c993c3d5d680c232d5e4db2cfdf2
Edwin <git@olback.net>
2017-07-08 04:09:13 +0200

    v1.4
    - Colors
    + isAdmin?
    * *SOME* cleanup in the code. Still needs work.

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #include <mem.h>
 #include <windows.h>
 
-char cversion[] = "1.4ds\n";
+char cversion[] = "1.4\n";
 char downlURL[] = "github.com/olback/gta-session/releases\n\n";
 int tabInTime = 5; // Time before the process begins.
 int time = 10; // Time before unblocking ports. Change to a higher value if it doesn't work. Change to a lower value if you get kicked.
@@ -21,65 +21,75 @@ int main() {
     /* Set window title. */
     SetConsoleTitle("GTAToggle");
 
-    /* Support for colored output */
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-    WORD saved_attributes;
-    /* Save current attributes */
-    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
-    saved_attributes = consoleInfo.wAttributes;
+    /* Is the user an administrator? */
+    system("powershell \" $user = [Security.Principal.WindowsIdentity]::GetCurrent();(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)\">isAdmin.txt");
+    static const char filenameAdmin[] = "isAdmin.txt";
+    FILE *isAdmin = fopen(filenameAdmin, "r");
+    if ( isAdmin != NULL ) {
+        char line[10]; /* Should only need to be 2 but that shit always returns false. */
+        while (fgets(line, sizeof line, isAdmin) != NULL) /* read a line */{
+            if (strcmp(line,"True\n") == 0) {
 
-    system("cls");
-    printf("Checking for updates...\n");
-    system("powershell -C \"curl https://raw.githubusercontent.com/olback/gta-session/master/VERSION -OutFile latest-version.txt\"");
-    sleep(1);
-    int lineNumber = 0;
-
-    static const char filename[] = "latest-version.txt";
-    FILE *file = fopen(filename, "r");
-    int count = 0;
-    if ( file != NULL ) {
-        char line[256]; /* or other suitable maximum line size */
-        while (fgets(line, sizeof line, file) != NULL) { /* read a line */
-            if (count == lineNumber) {
-                //use line or in a function return it
-                //in case of a return first close the file with "fclose(file);"
-
-                if (strcmp(line,cversion) == 0) {
-                    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
-                    printf("Already using latest version.\n\n");
-                    SetConsoleTextAttribute(hConsole, saved_attributes);
-                } else {
-                    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
-                    printf("New version available.\n");
-                    SetConsoleTextAttribute(hConsole, saved_attributes);
-                    printf("Download the latest version from %s", downlURL);
-                }
-
+                // Great, running as an administrator.
+                //printf("Is Admin!");
+                fclose(isAdmin);
+                system("del isAdmin.txt");
                 break;
 
             } else {
-                count++;
-            }
-        }
 
-        fclose(file);
-        system("del latest-version.txt");
+                printf("Not running as an administrator.\nExiting...\n");
+                fclose(isAdmin);
+                system("del isAdmin.txt");
+                sleep(2);
+                exit(0);
+
+            }
+
+        }
+    }
+
+    /* Check for updates */
+    printf("Checking for updates...\n");
+    system("powershell -C \"curl https://raw.githubusercontent.com/olback/gta-session/master/VERSION -OutFile latest-version.txt\"");
+    static const char filename[] = "latest-version.txt";
+    FILE *file = fopen(filename, "r");
+    if ( file != NULL ) {
+        char line[256]; /* or other suitable maximum line size */
+        while (fgets(line, sizeof line, file) != NULL) { /* read a line */
+                if (strcmp(line,cversion) == 0) {
+
+                    fclose(file);
+                    system("del latest-version.txt");
+                    printf("Already using latest version.\n\n");
+
+                } else {
+
+                    fclose(file);
+                    system("del latest-version.txt");
+                    printf("New version available.\n");
+                    printf("Download the latest version from %s", downlURL);
+
+                }
+
+                break;
+        }
 
     } else {
 
         printf("Error checking for updates. :(\n");
     }
 
+    /* Wait 5 seconds for the user to tab back in to GTA */
     printf("Waiting for %d",tabInTime);
     printf(" seconds. Tab back in to the game.\n\n");
     sleep(tabInTime);
 
-    // Removing all old rules named 'GTAToggle'.
+    /* Removing all old rules named 'GTAToggle'. */
     printf("Removing old firewall rules named 'GTAToggle'.\n");
     system("netsh advfirewall firewall del rule name=\"GTAToggle\" > NUL"); // Point output to NUL to suppress output from the command.
 
-    // Adding rules named 'GTAToggle'.
+    /* Adding rules named 'GTAToggle'. */
     printf("Adding firewall rules named 'GTAToggle'\n\n");
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=in action=block protocol=TCP localport=80,443 > NUL");
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=out action=block protocol=TCP localport=80,443 > NUL");
@@ -87,22 +97,22 @@ int main() {
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=out action=block protocol=UDP localport=6672,61455,61457,61456,61458 > NUL");
     system("netsh advfirewall firewall set rule name=\"GTAToggle\" new enable=no > NUL");
 
-    // Block ports.
+    /* Block ports. */
     printf("Blocking network traffic on these TCP ports: 80,443.\n");
     printf("Blocking network traffic on these UDP ports: 6672,61455,61457,61456,61458.\n\n");
     system("netsh advfirewall firewall set rule name=\"GTAToggle\" new enable=yes > NUL");
 
-    // Wait before unblocking ports.
+    /* Wait before unblocking ports. */
     printf("Waiting for %d",time);
     printf(" seconds.\n\n");
     sleep(time);
 
-    // Unblocking ports.
+    /* Unblocking ports. */
     printf("Unblocking network traffic on these TCP ports: 80,443.\n");
     printf("Unblocking network traffic on these UDP ports: 6672,61455,61457,61456,61458.\n\n");
     system("netsh advfirewall firewall set rule name=\"GTAToggle\" new enable=no > NUL");
 
-    // Clean up.
+    /* Clean up. */
     printf("Cleaning up.\n");
     system("netsh advfirewall firewall del rule name=\"GTAToggle\" > NUL");
 
