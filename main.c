0fa72ed9e5cc68e5b111df0f1e852bf2219a99ed
Edwin <git@olback.net>
2017-07-22 02:12:20 +0200

    1.6
    Just check the changes...

--- main.c
+++ main.c
@@ -6,82 +6,96 @@
  */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <mem.h>
 #include <windows.h>
 
-char cversion[] = "1.5\n";
-char downlURL[] = "github.com/olback/gta-session/releases\n\n";
-int tabInTime = 5; // Time before the process begins.
-int time = 10; // Time before unblocking ports. Change to a higher value if it doesn't work. Change to a lower value if you get kicked.
+static const char cversion[] = "1.6\n";                                         // Current version.
+static const char downlURL[] = "github.com/olback/gta-session/releases\n\n";    // Link to the latest release.
+static const int tabInTime = 5;                                                 // Time before the process begins. AKA the time you have to tab back in to the game.
+static const int time = 10;                                                     // Time to block ports. Increase if it doesn't work. Decrease if you get kicked from your session.
 
 int main() {
 
-    /* Set window title. */
+    // Set window title.
     SetConsoleTitle("GTAToggle");
 
-    /* Is the user an administrator? */
-    system("powershell \" $user = [Security.Principal.WindowsIdentity]::GetCurrent();(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)\">isAdmin.txt");
-    static const char filenameAdmin[] = "isAdmin.txt";
-    FILE *isAdmin = fopen(filenameAdmin, "r");
-    if ( isAdmin != NULL ) {
-        char line[10]; /* Should only need to be 2 but that shit always returns false. */
-        while (fgets(line, sizeof line, isAdmin) != NULL) /* read a line */{
-            if (strcmp(line,"True\n") == 0) {
 
-                // Great, running as an administrator.
-                //printf("Is Admin!");
-                fclose(isAdmin);
-                system("del isAdmin.txt");
-                break;
+    /* Check if the user is admin or not. Ref for popen: https://msdn.microsoft.com/en-us/library/96ayss4b.aspx */
+    /* Would like to remove this 'is admin?' part all together and replace is with a manifest. */
+    char psBuffer[128];
+    FILE *pPipe;
+
+    if ((pPipe = _popen(
+            "powershell \" $user = [Security.Principal.WindowsIdentity]::GetCurrent();(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)\"",
+            "rt")) == NULL) {
+        exit(1);
+    }
+
+    // Read pipe until end of file, or an error occurs.
+    while (fgets(psBuffer, 128, pPipe)) {
+
+        if (psBuffer[0] == 84) { // 84 is Decimal for the letter 'T' and 70 is the decimal for 'F'.
+
+            // Already running as an admin.
+
+        } else {
+
+            printf("Not running as an administrator.\nPrompting for permissions.\n");
+
+            system("powershell.exe Start-Process GTAToggle.exe -Verb runAs");
+
+            exit(0);
+
+        }
+
+    }
+
+    // Close pipe and print return value of pPipe.
+    if (feof(pPipe)) {
+        _pclose(pPipe);
+    }
+
+
+    /* Check for updates */
+    /* A nicer way to check for updates wouldn't hurt... */
+    printf("Checking for updates...\n");
+    system("powershell -C \"curl https://raw.githubusercontent.com/olback/gta-session/master/VERSION -OutFile latest-version\"");
+    static const char filenameLV[] = "latest-version";
+    FILE *latest_version = fopen(filenameLV, "r");
+    if (latest_version != NULL) {
+        char line[256]; /* or other suitable maximum line size */
+        while (fgets(line, sizeof line, latest_version) != NULL) { /* read a line */
+            if (strcmp(line, cversion) == 0) {
+
+                // Close file.
+                fclose(latest_version);
+
+                // Remove latest-version.
+                int del_lv = remove("latest-version");
+                if (del_lv != 0) {
+                    printf("Error removing latest-version.");
+                }
+                printf("Already using latest version.\n\n");
 
             } else {
 
-                fclose(isAdmin);
-                system("del isAdmin.txt");
-                printf("Not running as an administrator.\n");
-                /* For the love of god, please fix this... */
-                system("echo Set WshShell = WScript.CreateObject(\"WScript.Shell\") > start.vbs");
-                system("echo If WScript.Arguments.Length = 0 Then >> start.vbs");
-                system("echo   Set ObjShell = CreateObject(\"Shell.Application\") >> start.vbs");
-                system("echo   ObjShell.ShellExecute \"GTAToggle.exe\" _ >> start.vbs");
-                system("echo     , \"\"\"\" ^& WScript.ScriptFullName ^& \"\"\" RunAsAdministrator\", , \"runas\", 1 >> start.vbs");
-                system("echo   \nWScript.Quit >> start.vbs");
-                system("echo End if >> start.vbs");
-                system("C:\\Windows\\System32\\cscript.exe \"start.vbs\">NUL");
-                system("del start.vbs");
-                exit(0);
+                // Close file.
+                fclose(latest_version);
+
+                // Remove latest-version.
+                int del_lv = remove("latest-version");
+                if (del_lv != 0) {
+                    printf("Error removing latest-version.");
+                }
+
+                printf("New version available.\n");
+                printf("Download the latest version from %s", downlURL);
 
             }
 
-        }
-    }
-
-    /* Check for updates */
-    printf("Checking for updates...\n");
-    system("powershell -C \"curl https://raw.githubusercontent.com/olback/gta-session/master/VERSION -OutFile latest-version.txt\"");
-    static const char filename[] = "latest-version.txt";
-    FILE *file = fopen(filename, "r");
-    if ( file != NULL ) {
-        char line[256]; /* or other suitable maximum line size */
-        while (fgets(line, sizeof line, file) != NULL) { /* read a line */
-                if (strcmp(line,cversion) == 0) {
-
-                    fclose(file);
-                    system("del latest-version.txt");
-                    printf("Already using latest version.\n\n");
-
-                } else {
-
-                    fclose(file);
-                    system("del latest-version.txt");
-                    printf("New version available.\n");
-                    printf("Download the latest version from %s", downlURL);
-
-                }
-
-                break;
+            break;
         }
 
     } else {
@@ -89,9 +103,8 @@ int main() {
         printf("Error checking for updates. :(\n");
     }
 
-    /* Wait 5 seconds for the user to tab back in to GTA */
-    printf("Waiting for %d",tabInTime);
-    printf(" seconds. Tab back in to the game.\n\n");
+    /* Wait X amount of seconds for the user to tab back in to GTA */
+    printf("Waiting for %d seconds. Tab back in to the game.\n\n", tabInTime);
     sleep(tabInTime);
 
     /* Removing all old rules named 'GTAToggle'. */
@@ -99,7 +112,7 @@ int main() {
     system("netsh advfirewall firewall del rule name=\"GTAToggle\" > NUL"); // Point output to NUL to suppress output from the command.
 
     /* Adding rules named 'GTAToggle'. */
-    printf("Adding firewall rules named 'GTAToggle'\n\n");
+    printf("Adding firewall rules named 'GTAToggle'.\n\n");
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=in action=block protocol=TCP localport=80,443 > NUL");
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=out action=block protocol=TCP localport=80,443 > NUL");
     system("netsh advfirewall firewall add rule name=\"GTAToggle\" dir=in action=block protocol=UDP localport=6672,61455,61457,61456,61458 > NUL");
@@ -112,7 +125,7 @@ int main() {
     system("netsh advfirewall firewall set rule name=\"GTAToggle\" new enable=yes > NUL");
 
     /* Wait before unblocking ports. */
-    printf("Waiting for %d",time);
+    printf("Waiting for %d", time);
     printf(" seconds.\n\n");
     sleep(time);
 
@@ -128,5 +141,4 @@ int main() {
     sleep(2);
 
     return 0;
-
 }
